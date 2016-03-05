66d0956e91b571706532361fef221d9608570f9e
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-03-05 23:09:23 +0000

    Make format more like Google styleguide

--- src/FizzBuzzer.java
+++ src/FizzBuzzer.java
@@ -1,38 +1,57 @@
 import java.util.Scanner;
 
-
 public class FizzBuzzer {
 	public static void main(String[] args) {
-		for(int i=0; i<29;i++) {
+		for (int i = 0; i < 29;i++) {
 			System.out.println(respond(readKb()));
 		}
 	}
 
 	public static String respond(int n) {
-		String[] response={Integer.toString(n),"Fizz","Buzz","FizzBuzz"};
-		int x=0;
-		if(n%3 == 0) x+=1;
-		if(n%5 == 0) x+=2;
+		String[] response = {Integer.toString(n),
+				"Fizz",
+				"Buzz",
+				"FizzBuzz"};
+		int x = 0;
+		
+		if (n % 3 == 0) {
+			x += 1;
+		}
+		if (n % 5 == 0) {
+			x += 2;
+		}
 		return response[x];
 	}
 
 	// This overloads to allow us to play different versions
 	public static String respond(int n, int a, int b) {
-		String[] response={Integer.toString(n),"Fizz","Buzz","FizzBuzz"};
-		int x=0;
-		if(n%a == 0) x+=1;
-		if(n%b == 0) x+=2;
+		String[] response = {Integer.toString(n),
+				"Fizz",
+				"Buzz",
+				"FizzBuzz"};
+		int x = 0;
+		
+		if (n % a == 0) {
+			x += 1;
+		}
+		if (n % b == 0) {
+			x += 2;
+		}
 		return response[x];
 	}
 	
 	public static int readKb() {
-		int out=-1;
+		int out = -1;
 		Scanner s = new Scanner(System.in);
+		
 		try {
-		if(s.hasNextInt()) out=s.nextInt();
-		} catch(Exception IllegalStateException) {
+			if(s.hasNextInt()) {
+				out=s.nextInt();
+			}
+		} catch (Exception IllegalStateException) {
 			System.out.println("Looks like stdin is dead to us!");
 		}
+		
 		return out;		
 	}
 }
